4948e2bc91098bf518f96620ee4a7cf2d6e1235c
Randy Gelhausen <rgelhau@gmail.com>
2019-05-18 09:53:09 -0400

    Adding dask-cuda/dask-cudf test, README updates

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # rapids-test
-RAPIDS integration tests
+
+This repo is for integration tests for [nightly RAPIDS conda packages](https://anaconda.org/rapidsai-nightly).
+
+Test scripts focus on confirming functioning interaction between cross-project APIs.
+
+For example:
+
+1. Loading CSV data with cuStrings columns into a cuDF
+2. Finding DBSCAN clusters from a cuDF.
+3. Creating a DMatrix and training an XGBoost model from a cuDF.
+4. Creating a dask-cuda cluster and running a multi-column groupby over cuDF partitions.
---
+++ dask-cudf/python/test_daskcudf.py
@@ -0,0 +1,28 @@
+import pytest
+
+def test_cluster():
+    from dask_cuda import LocalCUDACluster
+    from dask.distributed import Client
+
+    cluster = LocalCUDACluster()
+    client = Client(cluster)
+    # this assumes running on a machine with more than one GPU
+    assert len(client.scheduler_info()['workers']) > 1
+
+def test_multicolumn_groupby():
+    import cudf, dask_cudf
+
+    tmp_df = cudf.DataFrame()
+    tmp_df['id'] = [0, 0, 1, 2, 2, 2]
+    tmp_df['val1'] = [0, 1, 0, 0, 1, 2]
+    tmp_df['val2'] = [9, 9, 9, 9, 9, 9]
+
+    ddf = dask_cudf.from_cudf(tmp_df, npartitions=2)
+    cudfSeries = ddf.groupby(['id', 'val1']).count().compute()
+
+    expectedValues = [1, 1, 1, 1, 1, 1]
+
+    assert len(expectedValues) == len(cudfSeries)
+
+    for (actual, expected) in zip(expectedValues, cudfSeries) :
+        assert actual == pytest.approx(expected)
